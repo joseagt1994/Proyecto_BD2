41c7c1428d508259defff77361fb3092b15bc228
joseagt1994 <josegt10100@gmail.com>
2019-05-12 17:48:15 -0600

    Login PHP

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php session_start(); ?>
+<html>
+
+<body>
+	<form action="<?php $_PHP_SELF?>" method="GET">
+
+		Usuario: <input type="text" name="usuario"/><br/>
+		Password: <input type="password" name="password"/><br/>
+		</br>
+		<input type="submit" value="Ingresar" />
+
+	</form>
+</body>
+
+</html>
+
+<?php
+
+	if($_GET["usuario"] || $_GET["password"]){
+
+		$m = new MongoDB\Driver\Manager("mongodb://192.168.1.24:27017");
+		$filter = ['nickname' => $_GET["usuario"], 'password' => $_GET["password"]];
+		$options = [];
+		$query = new MongoDB\Driver\Query($filter,$options);
+		$rows = $m->executeQuery('Proyecto.usuarios', $query);
+
+		if($rows->isDead()){ echo "El usuario no existe! Ingrese nuevamente."; }
+		else{
+			echo "Bienvenido!";
+			header("Location: menu.php");
+		}
+
+		//echo "Usuario: " . $rows . PHP_EOL;
+
+	}else { echo "Ingrese los datos necesarios para ingresar!"; }
+
+?>
