0ac8e4f5df9c556f7e42e7dfe4bd79bfa5a21095
joseagt1994 <josegt10100@gmail.com>
2019-05-12 18:11:54 -0600

    Menu Principal

--- menu.php
+++ menu.php
@@ -1,1 +1,17 @@
+<?php 
+	session_start();
 
+	if(!Isset($_SESSION['login'])){
+		$_SESSION['Redirect'] = $_SERVER['REQUEST_URL'];
+		header('Location: index.php');
+	}
+	
+?>
+<html>
+
+<body>
+	<h1> Menu Principal </h1>
+	<p> Bienvenido! </p><?php $_SESSION['login']?>
+</body>
+
+</html>
