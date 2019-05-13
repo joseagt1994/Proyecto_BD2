7b29178e90fb4898b2e7bed5eb4b8cae2816d860
joseagt1994 <josegt10100@gmail.com>
2019-05-12 18:27:38 -0600

    Acciones Menu

--- menu.php
+++ menu.php
@@ -4,9 +4,15 @@
 	if(!isset($_SESSION['login'])){
 		$_SESSION['Redirect'] = $_SERVER['REQUEST_URL'];
 		header('Location: index.php');
+	}else{
+		if(isset($_GET['tipo'])){
+			if($_GET['tipo'] == 1){ header('Location: usuarios.php'); }
+			else if($_GET['tipo'] == 2){ header('Location: productos.php'); }
+			else{ unset($_SESSION['login']); header('Location: index.php'); }
+		}
 	}
 	
-	print_r($_SESSION);
+	//print_r($_SESSION);
 
 ?>
 <html>
@@ -14,10 +20,20 @@
 <body>
 	<h1> Menu Principal </h1>
 	<p> Bienvenido! </p><?php $_SESSION['login']?>
-
-	<button>Usuarios</button>
-	<button>Productos</button>	
-
+	
+	<form action="<?php $_PHP_SELF?>" method="GET">
+		<input type="hidden" id="tipo" name="tipo"/>
+		<input type="submit" value="Usuarios" onclick="cambiar(1)"/>
+		<input type="submit" value="Productos" onclick="cambiar(2)"/>
+		<input type="submit" value="Cerrar sesion" onclick="cambiar(3)"/>	
+	</form>	
+	
 </body>
+
+<script>
+	function cambiar(valor){
+		document.getElementById("tipo").value = valor;
+	}
+</script>
 
 </html>
