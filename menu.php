3a83326adaedcd270790a0cf440c2ffc977f84a0
joseagt1994 <josegt10100@gmail.com>
2019-05-12 18:17:16 -0600

    Menu Principal

--- menu.php
+++ menu.php
@@ -1,17 +1,23 @@
 <?php 
 	session_start();
 
-	if(!Isset($_SESSION['login'])){
+	if(!isset($_SESSION['login'])){
 		$_SESSION['Redirect'] = $_SERVER['REQUEST_URL'];
 		header('Location: index.php');
 	}
 	
+	print_r($_SESSION);
+
 ?>
 <html>
 
 <body>
 	<h1> Menu Principal </h1>
 	<p> Bienvenido! </p><?php $_SESSION['login']?>
+
+	<button>Usuarios</button>
+	<button>Productos</button>	
+
 </body>
 
 </html>
